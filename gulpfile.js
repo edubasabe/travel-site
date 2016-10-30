252c41ceb780dc62d6a003465cae868babab908c
Edu <edubasabe1@gmail.com>
2016-10-29 19:17:04 -0500

    Introduccion a la estructura de archivos ideal para el desarrollo

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function(){
   console.log("Creaste un gulp task");
@@ -11,12 +12,12 @@ gulp.task('default', function(){
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   watch('./app/index.html', function() {
     gulp.start('html');
   });
